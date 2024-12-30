47fe2c61bf2ef4114e2a6bc0bd9a6796bab3ae2a
PutraGandaD <putraganda.dewata@gmail.com>
2024-12-30 22:54:11 +0700

    project: Initial livewire and tailwind setup

---
+++ resources/views/livewire/chat/index.blade.php
@@ -0,0 +1,11 @@
+<div class="fixed inset-0 flex h-full m-auto overflow-hidden bg-white border lg:shadow-sm lg:top-16 lg:inset-x-2 lg:h-[90%] rounded-t-lg">
+    <div class="relative w-full md:w-[320px] xl:w-[400px] overflow-y-auto shrink-0 h-full border">
+
+    </div>
+
+    <div class="relative hidden w-full h-full overflow-y-auto border-l md:grid" style="contain:content">
+        <div class="flex flex-col justify-center gap-3 m-auto text-center">
+            <h4 class="text-lg font-medium">Choose a conversation to start chatting</h4>
+        </div>
+    </div>
+</div>
