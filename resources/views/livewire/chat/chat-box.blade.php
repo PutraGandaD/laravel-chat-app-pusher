d980e1132ccae5f9ec709228cf45c2c4d5c7d378
PutraGandaD <putraganda.dewata@gmail.com>
2025-01-24 02:33:11 +0700

    project: Finished placeholder Frontend (+ chatbox footer)

--- resources/views/livewire/chat/chat-box.blade.php
+++ resources/views/livewire/chat/chat-box.blade.php
@@ -1,85 +1,122 @@
-<div>
-    {{-- header --}}
-    <header class="w-full sticky inset-x-0 flex pb-[5px] pt-[5px] top-0 z-10 bg-white border-b ">
-        <div class="flex items-center w-full gap-2 px-2 lg:px-4 md:gap-5">
+<div class="w-full overflow-hidden">
+    <div class="flex flex-col h-full overflow-y-scroll border-b grow">
 
-            <a href="shrink-0 hidden md:visible" href="#">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 12h-15m0 0l6.75 6.75M4.5 12l6.75-6.75" />
-                </svg>
-            </a>
+        {{-- header --}}
+        <header class="w-full sticky inset-x-0 flex pb-[5px] pt-[5px] top-0 z-10 bg-white border-b ">
+            <div class="flex items-center w-full gap-2 px-2 lg:px-4 md:gap-5">
 
-            {{-- avatar --}}
-            <div class="shrink-0">
-                <x-avatar class="h-9 w-9 lg:w-11 lg:h-11" />
+                <a href="shrink-0 hidden md:visible" href="#">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 12h-15m0 0l6.75 6.75M4.5 12l6.75-6.75" />
+                    </svg>
+                </a>
+
+                {{-- avatar --}}
+                <div class="shrink-0">
+                    <x-avatar class="h-9 w-9 lg:w-11 lg:h-11" />
+                </div>
+
+                <h6 class="font-bold truncate"> Yikes </h6>
+
+            </div>
+        </header>
+
+        {{-- body --}}
+        <main class="flex flex-col gap-3 p-2.5 overflow-y-auto  flex-grow overscroll-contain overflow-x-hidden w-full my-auto">
+
+            <div
+            @class([
+                'max-w-[85%] md:max-w-[78%] flex w-auto gap-2 relative mt-2',
+            ])>
+
+
+            <div @class(['shrink-0'])>
+
+                <x-avatar />
+
             </div>
 
-            <h6 class="font-bold truncate"> Yikes </h6>
+            {{-- message body --}}
+            <div @class(['flex flex-wrap text-[15px]  rounded-xl p-2.5 flex flex-col text-black bg-[#f6f6f8fb]',
+                            'rounded-bl-none border  border-gray-200/40 '=>false,
+                            'rounded-br-none bg-blue-500/80 text-white'=>true
+            ])>
 
-        </div>
-    </header>
-
-    {{-- body --}}
-    <main class="flex flex-col gap-3 p-2.5 overflow-y-auto  flex-grow overscroll-contain overflow-x-hidden w-full my-auto">
-
-        <div
-        @class([
-            'max-w-[85%] md:max-w-[78%] flex w-auto gap-2 relative mt-2',
-        ])>
-
-
-        <div @class(['shrink-0'])>
-
-            <x-avatar />
-
-        </div>
-
-        {{-- message body --}}
-        <div @class(['flex flex-wrap text-[15px]  rounded-xl p-2.5 flex flex-col text-black bg-[#f6f6f8fb]',
-                        'rounded-bl-none border  border-gray-200/40 '=>false,
-                        'rounded-br-none bg-blue-500/80 text-white'=>true
-        ])>
-
-        <p class="text-sm tracking-wide truncate whitespace-normal md:text-base lg:tracking-normal">
-            Lorem ipsum yakan?
-        </p>
-
-        <div class="flex gap-2 ml-auto">
-
-            <p @class([
-                'text-xs ',
-                'text-gray-500'=>false,
-                'text-white'=>true
-                ])>
-
-            5:25 am
-
+            <p class="text-sm tracking-wide truncate whitespace-normal md:text-base lg:tracking-normal">
+                Lorem ipsum yakan?
             </p>
 
-            {{-- message status , only show if message belongs auth --}}
-            <div>
-                {{-- double ticks --}}
-                <span @class('text-gray-200')>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check2-all" viewBox="0 0 16 16">
-                        <path d="M12.354 4.354a.5.5 0 0 0-.708-.708L5 10.293 1.854 7.146a.5.5 0 1 0-.708.708l3.5 3.5a.5.5 0 0 0 .708 0l7-7zm-4.208 7-.896-.897.707-.707.543.543 6.646-6.647a.5.5 0 0 1 .708.708l-7 7a.5.5 0 0 1-.708 0z"/>
-                        <path d="m5.354 7.146.896.897-.707.707-.897-.896a.5.5 0 1 1 .708-.708z"/>
-                    </svg>
-                </span>
+            <div class="flex gap-2 ml-auto">
 
-                {{-- single ticks --}}
-                {{-- <span @class('text-gray-200')>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check2" viewBox="0 0 16 16">
-                        <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z"/>
-                    </svg>
-                </span> --}}
+                <p @class([
+                    'text-xs ',
+                    'text-gray-500'=>false,
+                    'text-white'=>true
+                    ])>
+
+                5:25 am
+
+                </p>
+
+                {{-- message status , only show if message belongs auth --}}
+                <div>
+                    {{-- double ticks --}}
+                    <span @class('text-gray-200')>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check2-all" viewBox="0 0 16 16">
+                            <path d="M12.354 4.354a.5.5 0 0 0-.708-.708L5 10.293 1.854 7.146a.5.5 0 1 0-.708.708l3.5 3.5a.5.5 0 0 0 .708 0l7-7zm-4.208 7-.896-.897.707-.707.543.543 6.646-6.647a.5.5 0 0 1 .708.708l-7 7a.5.5 0 0 1-.708 0z"/>
+                            <path d="m5.354 7.146.896.897-.707.707-.897-.896a.5.5 0 1 1 .708-.708z"/>
+                        </svg>
+                    </span>
+
+                    {{-- single ticks --}}
+                    {{-- <span @class('text-gray-200')>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check2" viewBox="0 0 16 16">
+                            <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z"/>
+                        </svg>
+                    </span> --}}
+                </div>
+
+
             </div>
 
 
-        </div>
+        </main>
 
+        {{-- footer (send message) --}}
+        <footer class="inset-x-0 z-10 bg-white shrink-0">
+            <div class="p-2 border-t">
+                <form method="POST" autocapitalize="off">
+                    @csrf
 
-    </main>
+                    <input type="hidden" autocomplete="false" style="display:none">
 
-    {{-- footer --}}
+                    <div class="grid grid-cols-12">
+                        <input
+                                x-model="body"
+                                type="text"
+                                autocomplete="off"
+                                autofocus
+                                placeholder="write your message here"
+                                maxlength="1700"
+                                class="col-span-10 bg-gray-100 border-0 rounded-lg outline-0 focus:border-0 focus:ring-0 hover:ring-0 focus:outline-none"
+                        >
+
+                        <button x-bind:disabled="!body.trim()"  class="col-span-2" type='submit'>Send</button>
+
+                    </div>
+
+                </form>
+
+                @error('body')
+
+                <p> {{$message}} </p>
+
+                @enderror
+
+            </div>
+
+        </footer>
+
+    </div>
 
 </div>
