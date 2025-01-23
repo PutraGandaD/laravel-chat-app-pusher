00e07b77e01832252e2da8d91cc90361ee02d6f1
PutraGandaD <putraganda.dewata@gmail.com>
2025-01-24 00:55:32 +0700

    project: Added chat-list placeholder and initial chat-box

--- resources/views/livewire/chat/index.blade.php
+++ resources/views/livewire/chat/index.blade.php
@@ -1,6 +1,6 @@
 <div class="fixed inset-0 flex h-full m-auto overflow-hidden bg-white border lg:shadow-sm lg:top-16 lg:inset-x-2 lg:h-[90%] rounded-t-lg">
     <div class="relative w-full md:w-[320px] xl:w-[400px] overflow-y-auto shrink-0 h-full border">
-
+        <livewire:chat.chat-list>
     </div>
 
     <div class="relative hidden w-full h-full overflow-y-auto border-l md:grid" style="contain:content">
