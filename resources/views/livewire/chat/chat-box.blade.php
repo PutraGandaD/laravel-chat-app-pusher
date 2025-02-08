886c22e586dcd6da82f10198a4f4f55c191d7462
PutraGandaD <putraganda.dewata@gmail.com>
2025-02-09 02:56:50 +0700

    feat(chat) : Fix back arrow

--- resources/views/livewire/chat/chat-box.blade.php
+++ resources/views/livewire/chat/chat-box.blade.php
@@ -1,4 +1,4 @@
-<divsi
+<div
     @scroll-bottom.window="$nextTick(() => {
         let conversationElement = document.getElementById('conversation');
         if (conversationElement) {
@@ -13,7 +13,7 @@
         <header class="w-full sticky inset-x-0 flex pb-[5px] pt-[5px] top-0 z-10 bg-white border-b ">
             <div class="flex items-center w-full gap-2 px-2 lg:px-4 md:gap-5">
 
-                <a href="shrink-0 hidden md:visible" href="#">
+                <a class="shrink-0 lg:hidden" href="{{ route('chat.index') }}">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 12h-15m0 0l6.75 6.75M4.5 12l6.75-6.75" />
                     </svg>
